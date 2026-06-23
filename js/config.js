851d24bb5851c6f1eb0ae428f2018c0dfe2635c9
wladimir1003 <83191686+wladimir1003@users.noreply.github.com>
2026-06-23 15:52:43 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 /*
-ElectroLíneas Chile GPS
+Líneas Eléctricas Chile V3
 Creado por Wladimir Campos
 www.JFSasesorias.org
 */
 const APP_CONFIG = {
-  name: "ElectroLíneas Chile GPS",
-  stage: "Etapa 3",
+  name: "Líneas Eléctricas Chile V3",
+  stage: "V3",
   author: "Wladimir Campos",
   website: "www.JFSasesorias.org",
   urls: {
@@ -13,7 +13,7 @@ const APP_CONFIG = {
     subestaciones: "https://ide-energia.minenergia.cl/server/rest/services/IDE_Energia/Visor_IDE_Energ%C3%ADa/MapServer/8/query"
   },
   db: {
-    name: "electrolineas_chile_gps_db",
+    name: "Líneas Eléctricas Chile V3",
     version: 1,
     storeName: "capas"
   }
